b23ae1431454e3362e37b4f0d1024ba177efa002
Octavio Haeublein <33793740+OctavioHaeublein@users.noreply.github.com>
2017-11-18 21:56:47 -0300

    Add files via upload

--- Porton.ino
+++ Porton.ino
@@ -1,17 +1,19 @@
-#define abierto 1
-#define cerrado 2
-#define parado 3
-#define abriendo 4
-#define cerrando 5
+#define parado 1
+#define abriendo 2
+#define cerrando 3
+#define obstaculo 4
  
-char comando='d';
-char estado;
+char comando='d';   //variable para almacenar el dato entrante
+char estado; //variable de estados
 int motor= 8;  //motor de 12vc
 int vcc= 9;  //Alimentacion motor
 int fc=11;  // final de carrera
 int fcc=12;  //final de carrera 2
 int irled =10; //led infrarrojo
 int ireciver =7; //receptor infrarrojo
+int sirena = 6; // Sirena para el portón (5v)
+int leda = 5; //indicador para cuando abrió
+int ledc = 4; // indicador para cuando cerró
 
 void setup() {
  Serial.begin(9600);
@@ -21,15 +23,15 @@ void setup() {
  pinMode(fcc,INPUT);
  pinMode(fc,INPUT);
  pinMode(ireciver,INPUT);
- pinMode(5,OUTPUT);
- pinMode(4,OUTPUT);
- pinMode(3,OUTPUT);
+ pinMode(leda,OUTPUT);
+ pinMode(ledc,OUTPUT);
+ pinMode(sirena,OUTPUT);
 }
 
 void loop() {
  if(Serial.available()>0){
   comando=Serial.read();
-  Serial.println(comando);
+  Serial.println(comando); //recibe el dato bluetooth y lo almacena
 }
  switch (comando){
   case 'a':
@@ -41,55 +43,57 @@ void loop() {
   case 'c':
   estado=parado;
   break;
-  
 }
  switch (estado){
-  case abierto:
-  digitalWrite(vcc,LOW);
-  digitalWrite(3, LOW);
-  digitalWrite(irled,LOW);
-  break;
-  case cerrado:
-  digitalWrite(vcc,LOW);
-  digitalWrite(3, LOW);
-  digitalWrite(irled,LOW);
-  break;
   case parado:
   digitalWrite(vcc,LOW);
-  digitalWrite(3, LOW);
   digitalWrite(irled,LOW);
+  digitalWrite(sirena, LOW);
+  break;
+  case obstaculo:
+  digitalWrite(motor,HIGH);
+  digitalWrite(vcc,HIGH);
+  digitalWrite(irled,HIGH);
+  digitalWrite(sirena, HIGH);
+  digitalWrite(ledc,HIGH);
+  digitalWrite(leda,HIGH);
   break;
   case abriendo:
   digitalWrite(motor,HIGH);
   digitalWrite(vcc,HIGH);
   digitalWrite(irled,HIGH);
-  digitalWrite(3, HIGH);
-  digitalWrite(4,LOW);
-  digitalWrite(5,LOW);
+  digitalWrite(sirena, HIGH);
+  digitalWrite(ledc,LOW);
+  digitalWrite(leda,LOW);
   break;
   case cerrando:
   digitalWrite(motor, LOW);
   digitalWrite(vcc,HIGH);
   digitalWrite(irled,HIGH);
-  digitalWrite(5,LOW);
-  digitalWrite(4,LOW);
-  digitalWrite(3, HIGH);
+  digitalWrite(sirena, HIGH);
+  digitalWrite(ledc,LOW);
+  digitalWrite(leda,LOW);
   break;
 }
   if((digitalRead(fcc)>0) && (estado == abriendo)){
     comando='c';
-    Serial.println("Final de carrera 1");
-    digitalWrite(4,HIGH);
+    Serial.println("Abierto");
+    digitalWrite(leda,HIGH);
     }
-
+  if((digitalRead(fcc)>0) && (estado == obstaculo)){
+    digitalWrite(vcc,LOW);
+    delay(10000);
+    comando='b';
+    Serial.println("Intentado cerrar...");
+    }
   if((digitalRead(fc)>0) && (estado == cerrando)){
     comando='c';
-    Serial.println("Final de carrea 2");
-    digitalWrite(5,HIGH);
+    Serial.println("Cerrado");
+    digitalWrite(ledc,HIGH);
     }    
-    int b =digitalRead(ireciver);
-   if(((estado==abriendo)|| (estado==cerrando)) && b != 0){ 
+    int b =digitalRead(ireciver);  //recive la lectura del receptor infrarrojo
+   if(estado==cerrando && b != 0){ 
     comando='c';
-    Serial.println(b);
-   Serial.println("Led infrarrojo");}
+    Serial.println("Obstaculo encontrado");
+    }
 }
